1174f5574aa755d8d5005d28ca8711e3d2ae685c
Trojan295 <trojan295@gmail.com>
2017-10-05 12:19:24 +0200

    Prepared to pre-release

--- setup.py
+++ setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from distutils.core import setup
 
 setup(name='python-uma',
       version='0.1',
       description='UMA Resource Server implementation for Python 3',
       url='https://github.com/trojan295/python_uma',
+      download_url='https://github.com/Trojan295/python_uma/archive/0.1.tar.gz',
       author='Damian Czaja',
       author_email='trojan295@gmail.com',
       license='MIT',
       packages=['uma'],
-      zip_safe=False
+      keywords=['authorization', 'uma', 'oauth', 'oidc', 'security'],
       )
